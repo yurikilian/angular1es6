57cf517fa183ffbeae63036ac059afe44969f88f
Yuri Kilian <ykilian2006@gmail.com>
2016-09-15 23:13:25 -0300

    corrigido ordem de carregamento dos polyfills

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,8 @@ var webpack = require('webpack');
 
 module.exports = {
     entry: [
-        path.resolve(__dirname, 'src/app/app.js'),
-        path.resolve(__dirname, 'src/vendor/app.vendor.js')
+      path.resolve(__dirname, 'src/vendor/app.vendor.js'),
+        path.resolve(__dirname, 'src/app/app.js')
     ],
     output: {
         path: path.resolve(__dirname, 'dist'),
